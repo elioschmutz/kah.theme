2b0a6d720ccd582acf9e7b669eb1c9e9930f978b
elio.schmutz <elio.schmutz@gmail.com>
2017-03-22 22:08:23 +0100

    Add german translation

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'kah_jupiter_child_enqueue_styles' );
-function kah_jupiter_child_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
+function child_theme_slug_setup() {
+    load_child_theme_textdomain( 'mk_framework', get_stylesheet_directory() . '/languages' );
 }
-?>
+add_action( 'after_setup_theme', 'child_theme_slug_setup' );
